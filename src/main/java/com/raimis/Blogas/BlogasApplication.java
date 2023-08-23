f2d9a141fc2a793d3976f488ce46e920238337e2
Raimondas <raimondas@gmail.com>
2023-08-23 20:42:07 +0300

    First commit

---
+++ src/main/java/com/raimis/Blogas/BlogasApplication.java
@@ -0,0 +1,13 @@
+package com.raimis.Blogas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogasApplication.class, args);
+	}
+
+}
